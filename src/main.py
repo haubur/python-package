0faacd7703953d6e0caea77a3ede0ea022d50523
haubur <hk.burde@gmail.com>
2024-03-08 12:00:26 +0100

    test templating

---
+++ src/main.py
@@ -0,0 +1,29 @@
+"""
+Document module.
+"""
+
+def fibonacci(index: int) -> int:
+    """
+    Document function.
+    """
+
+    if index == 0:
+        result = 0
+    elif index == 1:
+        result = 1
+    elif index > 1:
+        result = fibonacci(index - 1) + fibonacci(index - 2)
+
+    return result
+
+def main() -> None:
+    """
+    Document function.
+    """
+
+    for index in range(0,10):
+        template = "fibonacci({index}) = {result}"
+        print(template.format(iindex = index, result = fibonacci(index)))
+
+if __name__ == "__main__":
+    main()
---
+++ tests/test_main/test_main.py
@@ -0,0 +1,27 @@
+import pytest
+
+from src.main import main
+from src.main import fibonacci
+
+class TestMainOperations:
+
+    @pytest.mark.dependency()
+    def test_fibonacci_generator(self):
+        """
+        Document test.
+        """
+
+        assert fibonacci(-99) is None
+        assert fibonacci(9) == 34
+
+    @pytest.mark.dependency(
+        depends = [
+            "TestMainOperations::test_fibonacci_generator"
+        ]
+    )
+    def test_main_executes_properly(self):
+        """
+        Document test.
+        """
+
+        assert main() is None
